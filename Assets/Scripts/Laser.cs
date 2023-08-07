6e73ed949788fb777eb6e7155d3a7f39a0050254
Aaron Michelson <ajmichel4000@gmail.com>
2023-08-07 11:38:13 -0500

    Completed Prototype

---
+++ Assets/Scripts/Laser.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    // speed variable of 8
+
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // translate laser up
+
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+
+        // if laser position is greater than 7 on the y, then destroy the object
+
+        if (transform.position.y > 7f)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 3.5f;
+    [SerializeField]
+    private GameObject _laserPrefab;
+    [SerializeField]
+    private float _fireRate = 0.5f;
+    private float _canFire = -1f;
+    [SerializeField]
+    private int _lives = 3;
+
+    private SpawnManager _spawnManager;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, -1, 0);
+
+        // find the object and get the component
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+
+        if (_spawnManager == null)
+        {
+            Debug.LogError("The Spawn Manager is NULL.");
+        }
+    }
+
+    void Update()
+    {
+        CalculateMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > _canFire)
+        {
+            FireLaser();
+        }
+    }
+
+    void CalculateMovement ()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+
+        transform.Translate(direction * _speed * Time.deltaTime);
+
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -5f, -1f), 0);
+
+        if (transform.position.x > 10.5f)
+        {
+            transform.position = new Vector3(-10.5f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -10.5f)
+        {
+            transform.position = new Vector3(10.5f, transform.position.y, 0);
+        }
+    }
+
+    void FireLaser()
+    {
+            _canFire = Time.time + _fireRate;
+
+            Instantiate(_laserPrefab, transform.position + new Vector3(0, 1.05f, 0), Quaternion.identity);   
+    }
+
+    public void Damage()
+    {
+
+        // _lives = _lives - 1;
+        // _lives -= 1;
+
+        _lives--;
+
+        // check if dead, and if we are dead, destroy us
+
+        if (_lives < 1)
+        {
+            // Communicate with spawn manager, and let them know to stop spawning
+
+            _spawnManager.OnPlayerDeath();
+
+            Destroy(this.gameObject);
+        }
+    }
+}
