fb89ae1ab740e8ee399fafd0c8e2428f5759c980
Aaron Michelson <ajmichel4000@gmail.com>
2023-11-19 11:39:36 -0600

    Implemented enemy explosions

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -10,10 +10,24 @@ public class Enemy : MonoBehaviour
 
     private Player _player;
 
+    private Animator _anim;
+
     // Start is called before the first frame update
     void Start()
     {
-        _player = GameObject.Find("Player").GetComponent<Player>();    
+        _player = GameObject.Find("Player").GetComponent<Player>();
+
+        if (_player == null)
+        {
+            Debug.LogError("The Player is NULL.");
+        }
+
+        _anim = GetComponent<Animator>();
+
+        if (_anim == null)
+        {
+            Debug.LogError("The Animator is NULL");
+        }
     }
 
     // Update is called once per frame
@@ -41,7 +55,11 @@ public class Enemy : MonoBehaviour
                 player.Damage();
             }
 
-            Destroy(this.gameObject);
+            _anim.SetTrigger("OnEnemyDeath");
+
+            _speed = 2.5f;
+
+            Destroy(this.gameObject, 2.8f);
         }
 
         if (other.tag == "Laser")
@@ -53,7 +71,11 @@ public class Enemy : MonoBehaviour
                 _player.AddScore(10);
             }
 
-            Destroy(this.gameObject);
+            _anim.SetTrigger("OnEnemyDeath");
+
+            _speed = 2.5f;
+
+            Destroy(this.gameObject, 2.8f);
         }
     }
 }
